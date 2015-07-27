c8b46878e7bbfa094d46e3c957fae529d17be476
fgimenez <fgimenez@hispasat.es>
2015-07-27 13:58:33 +0200

    added comentarios al AJAX que gestiona la edicion de partidos

--- porrasite/static/js/mundial-ajax.js
+++ porrasite/static/js/mundial-ajax.js
@@ -15,24 +15,49 @@ $( document ).ready(function() {
 
 	$('.edit_ajax').click(function()
 	{
-		console.log("actualizando partido");
+		//console.log("actualizando partido");
 		var partidoid;
 		var local;
 		var visitante;
+
+		/*
+			Primero cojemos el atributo partidoid, que viene incluido en el template 
+			grupo_equipos.html en la parte del boton.
+			Cuando creamos el boton incluimos un parametro llamado data-partidoid
+			y le pasamos el dato en formato django {{partidoid}}
+		*/
 		partidoid = $(this).attr("data-partidoid");
+
+		/*
+			Creamos 2 variables que almacenen las etiquetas correctas para saber de donde se tiene
+			que traer los valores a cambiar,
+			Estos identificadores se han puesto en el template grupo_equipos al declarar los input
+			tambien con el tag del tipo django {{tag}}
+		*/
 		id_local = '#gol_local_' + partidoid;
 		id_visitante = '#gol_visitante_' + partidoid;
-		//console.log('Partidoid: ' + partidoid + ' local: '+local + ' visitante: ' + visitante);
+		
+		// Obtenemos ahora los nuevos valores del resultado del partido que queremos modificar
 		local = $(id_local).val();
 		visitante = $(id_visitante).val();
-		console.log('nuevo local:' + local);
-		console.log('nuevo visitante:' + visitante);
+		
+		/*
+			Ahora llamamos a la URL declarada en django em urls.py con tipo: get
+			y le pasamos como parametros get:
+				- El id del partido
+				- Los nuevos goles del local y del visitante
+		*/
+
 		$.get('/mundial/edita_partido_ajax/', {partido_id: partidoid,
 												local: local,
 												visitante: visitante,
-												}, function(data)
+												});
+		/*, function(data)
 		{
-			//console.log("llego dentro del get del javascript");
+			
+				lo que hacemos aqui es actualizar los propios elementos input con los nuevos valores
+				aunque yo creo que no haria falta...
+			
 			var etiqueta_ida = '#gol_local_' + partidoid;
 			var etiqueta_vuelta = '#gol_visitante_' + partidoid;
 			//console.log(etiqueta);
@@ -42,5 +67,6 @@ $( document ).ready(function() {
 			$(etiqueta_ida).html(local);
 			$(etiqueta_vuelta).html(visitante);		
 		});
+		*/
 	});
 });
